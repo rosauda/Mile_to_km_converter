3f27abb090fbf12a6615389acba6077e24c79438
rodrigo.sauda <rodrigo.sauda@hotmail.com>
2023-01-20 18:36:39 +0100

    Starting

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# starting
+import tkinter
+
+
+# Creating window
+window = tkinter.Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=500)
+
+# Creating label
+my_label = tkinter.Label(text="Hello I am Rodrigo Sauda, how are you?", font=("Arial", 12, "bold"))
+my_label.pack()
+
+
+window.mainloop()
+
